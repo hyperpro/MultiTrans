e65908fc34819c4781e4d228e9c6f9c3c9a81601
zhangbjb <zhangbjb@126.com>
2013-10-08 17:58:15 +0800

    ABCD

--- Fileserver/src/com/fudan/sonic/fileServer/DataType.java
+++
@@ -1,8 +0,0 @@
-package com.fudan.sonic.fileServer;
-import java.nio.charset.Charset;
-
-public class DataType {
-
-	public final static Charset charset = Charset.forName("UTF-8");
-	public final static int UPLOAD_FILE  = 1;
-}
