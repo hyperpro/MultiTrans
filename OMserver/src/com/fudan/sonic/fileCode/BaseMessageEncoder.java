0a67bd58efe9d0d5fab8c1151d22841c88cea21c
zhangbjb <zhangbjb@126.com>
2013-10-12 13:32:23 +0800

    >>>

--- OMserver/src/com/fudan/sonic/fileCode/BaseMessageEncoder.java
+++ OMserver/src/com/fudan/sonic/fileCode/BaseMessageEncoder.java
@@ -2,6 +2,7 @@ package com.fudan.sonic.fileCode;
 
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
 
 import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.session.IoSession;
@@ -11,18 +12,20 @@ import org.apache.mina.filter.codec.demux.MessageEncoder;
 import com.fudan.sonic.DataType.DataType;
 
 public class BaseMessageEncoder implements MessageEncoder<BaseMessage>{
-
+	
+	
 	@Override
 	public void encode(IoSession session, BaseMessage message,
 			ProtocolEncoderOutput outPut) throws Exception {
 		// TODO Auto-generated method stub
+		CharsetEncoder newEncoder = DataType.charset.forName("UTF-8").newEncoder();
 		IoBuffer buffer = IoBuffer.allocate(8);
 		buffer.setAutoExpand(true);
 		buffer.putInt(message.getDataType());
 		NewFile file = (NewFile) message.getData();
 		byte[] byteStr = file.getUserName().getBytes(DataType.charset);
 		buffer.putInt(byteStr.length);
-		buffer.putInt();
+		buffer.putString(arg0, arg1)
 	}
 
 }
